a97dda814db17aff8798086bc3b705ec081c579e
Renato <renato.chero@tecsup.edu.pe>
2025-08-27 19:11:32 -0500

    asda

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=dpg-d2nmbpruibrs73f5els0-a.oregon-postgres.render.com";
+	$host = "host=dpg-d2nmboruibrs73f5ekjg-a.oregon-postgres.render.com";
 	$port = "port=5432";
-	$dbname = "dbname=db1_q6hn";
-	$user = "user=db1_q6hn_user";
-	$password = "password=JKblEhD8zM2dXDDqDSVfOfBRIW9zct3a";
+	$dbname = "exampledbnube2_706n";
+	$user = "exampledbnube2_706n_user";
+	$password = "exampledbnube2_706n_user";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
