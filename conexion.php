e756f5da33c4ddf061f7361212ced6ae077e6253
Renato <renato.chero@tecsup.edu.pe>
2025-08-27 19:02:35 -0500

    ser

--- conexion.php
+++ conexion.php
@@ -1,15 +1,17 @@
 <?php
+function conexion(){
+    $conn_string = "host=dpg-d2nmboruibrs73f5ekjg-a.oregon-postgres.render.com ".
+                   "port=5432 ".
+                   "dbname=exampledbnube2_706n ".
+                   "user=exampledbnube2_706n_user ".
+                   "password=Y2sH4BZycoqYQ5qc6uurhxF7mPI7B3VL ".
+                   "sslmode=require";
 
-	function conexion(){
+    $db = pg_connect($conn_string);
 
-	$host = "dpg-d2nmboruibrs73f5ekjg-a.oregon-postgres.render.com";
-	$port = "port=5432";
-	$dbname = "dbname=exampledbnube2_706n";
-	$user = "user=exampledbnube2_706n_user";
-	$password = "password=Y2sH4BZycoqYQ5qc6uurhxF7mPI7B3VL";
-
-	$db = pg_connect("$host $port $dbname $user $password");
-
-	return $db;
+    if (!$db) {
+        die("Error de conexión: " . pg_last_error());
+    }
+    return $db;
 }
 ?>
